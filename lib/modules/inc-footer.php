0f0abe0b1a77f6bad242b5a11cd6232018d15033
Ilya <86732040+Gitsans66@users.noreply.github.com>
2021-11-14 22:11:21 +0300

    Add files via upload

---
+++ lib/modules/inc-footer.php
@@ -0,0 +1,85 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class="footer-ico-logo darkmode-footer-logo">
+					</div>
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Main</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/download">
+										<span>Download</span>
+										</a>
+									</div>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>GitHub</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://github.com/GitSans66/MinOS" target="_blank">
+										<span>Repository</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						<div class="footer-con-side">
+							<div class="footer-tx1-founder">
+								<div class="footer-ico-developer" style="background: url('/img/icons/footer/dh.png') center center / cover no-repeat;">
+								</div>
+								<div class="footer-ico-developer" style="background: url('/img/icons/footer/hykem.png') center center / cover no-repeat;">
+								</div>
+								<a href="/about">
+								<span>Founded by DH and Hykem</span>
+								</a>
+							</div>
+						</div>
+						<div class="footer-con-side">
+							<div class="footer-tx1-webmaster">
+								<div class="footer-ico-developer" style="background: url('/img/users/daginatsuko.png') center / cover no-repeat;">
+								</div>
+								<a href="https://daginatsuko.com" target="_blank">
+								<span>Website by DAGINATSUKO © 2021</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
